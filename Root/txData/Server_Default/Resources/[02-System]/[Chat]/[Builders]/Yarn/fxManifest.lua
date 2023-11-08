fb46fc6fa1e7c8329e8b27181dc4032d41c17c5e
Matthew T. Demarey <clark1122@live.com>
2023-11-08 18:57:11 -0500

    Create Yarn's fxManifest.lua
    Add Yarn fxManifest Lua File.
    
    Signed-off-by: Matthew T. Demarey <clark1122@live.com>

---
+++ Root/txData/Server_Default/Resources/[02-System]/[Chat]/[Builders]/Yarn/fxManifest.lua
@@ -0,0 +1,22 @@
+FX_Version "Cerulean"
+Lua54 "Yes"
+Games {
+  "Common",
+  "GTA4",
+  "GTA5",
+  "RDR3"
+}
+Dependencies {
+  "/OneSync",
+  "/GameBuild:2944",
+  "/Server:6683"
+}
+
+Version "1.0.0"
+Author "CFX.re <root@cfx.re> | Edited by Iridescent Wolves Development Team"
+Description "Builds Resources With Yarn. To Learn More Visit This Page: https://classic.yarnpkg.com"
+Repository "N/A"
+
+Server_Scripts {
+  "WebData/yarn_builder.js"
+}
