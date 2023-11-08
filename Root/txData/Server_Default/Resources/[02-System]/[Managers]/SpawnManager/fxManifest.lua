b3f879e2fd2533afb76a164c7a847774b4bdbb6a
Matthew T. Demarey <clark1122@live.com>
2023-11-08 18:49:18 -0500

    Create SpawnManager's fxManifest.lua
    Add SpawnManager fxManifest Lua File.
    
    Signed-off-by: Matthew T. Demarey <clark1122@live.com>

---
+++ Root/txData/Server_Default/Resources/[02-System]/[Managers]/SpawnManager/fxManifest.lua
@@ -0,0 +1,20 @@
+FX_Version "Cerulean"
+Lua54 "Yes"
+Games {
+  "GTA5",
+  "RDR3"
+}
+Dependencies {
+  "/OneSync",
+  "/GameBuild:2944",
+  "/Server:6683"
+}
+
+Version "1.0.0"
+Author "CFX.re <root@cfx.re> | Edited by Iridescent Wolves Development Team"
+Description "Handles Spawning Players In A Unified Fashion, Preventing Resources From Having To Implement Custom Spawn Logic."
+Repository "N/A"
+
+Client_Scripts {
+  "fxData/Scripts/Lua/Client/SpawnManager_Cl.lua"
+}
