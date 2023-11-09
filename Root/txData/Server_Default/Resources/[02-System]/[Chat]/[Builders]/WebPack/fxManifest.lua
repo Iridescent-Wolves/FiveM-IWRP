af4701f83103734e6a4c19bb322a1f94bd6f2c32
Matthew T. Demarey <clark1122@live.com>
2023-11-08 19:01:19 -0500

    Create WebPack's fxManifest.lua
    Add WebPack fxManifest Lua File.
    
    Signed-off-by: Matthew T. Demarey <clark1122@live.com>

---
+++ Root/txData/Server_Default/Resources/[02-System]/[Chat]/[Builders]/WebPack/fxManifest.lua
@@ -0,0 +1,23 @@
+FX_Version "Cerulean"
+Lua54 "Yes"
+Games {
+  "Common",
+  "GTA4",
+  "GTA5",
+  "RDR3"
+}
+Dependencies {
+  "/OneSync",
+  "/GameBuild:2944",
+  "/Server:6683",
+  "Yarn"
+}
+
+Version "1.0.0"
+Author "CFX.re <root@cfx.re> | Edited by Iridescent Wolves Development Team"
+Description "Builds Resources With WebPack. To Learn More Visit This Page: https://webpack.js.org"
+Repository "N/A"
+
+Server_Scripts {
+  "WebData/webpack_builder.js"
+}
